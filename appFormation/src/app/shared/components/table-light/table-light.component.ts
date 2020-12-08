1339763bb0967755b1a99000169c1a9eff0e2965
Jean-Christophe AUDARD <jc.audard@hotmail.fr>
2020-12-08 14:57:48 +0100

    correctif table-light

--- appFormation/src/app/shared/components/table-light/table-light.component.ts
+++ appFormation/src/app/shared/components/table-light/table-light.component.ts
@@ -7,21 +7,24 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TableLightComponent implements OnInit {
 
-  // Les lignes du tableau
+  // Une collection d'objets d'un certain type d'entité (ex : Order, Client, ...)
   @Input() public collection:any[];
 
-  // Liste des entêtes du tableau
+  // Liste des entêtes du tableau pour ce type d'entité
   @Input() public headers:string[];
 
+  // Tableau des propriétés associées à ce type d'entité
   public collectionProps:string[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
     if (this.collection.length>0)
+      // On initialise par introspection des propriétés du 1er objet de la collection, le tableau des propriétés de ce type d'entité
       this.collectionProps = Object.getOwnPropertyNames(this.collection[0]);
   }
 
+  // Pour récupérer la propriété d'un objet (avec la propertyName dynamique)
   public getProperty(obj:any, propertyName:string)
   {
     return (obj[propertyName]).toString();
