534b98aab7f52c39eeb240b5325f6451a77cccb4
Jean-Christophe AUDARD <jc.audard@hotmail.fr>
2020-12-09 11:37:28 +0100

    Ajout de l'encapsulation NONE (au lieu de EMULATE par défaut...) dans le décorateur @Component du composant TableLight pour pouvoir exploiter ses propres classes CSS plutôt que celles du composant qui utilise ce TableLight

--- appFormation/src/app/shared/components/table-light/table-light.component.ts
+++ appFormation/src/app/shared/components/table-light/table-light.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Client } from '../../models/client.model';
 import { Order } from '../../models/order.model';
 
 @Component({
   selector: 'app-table-light',
   templateUrl: './table-light.component.html',
-  styleUrls: ['./table-light.component.scss']
+  styleUrls: ['./table-light.component.scss'],
+  encapsulation : ViewEncapsulation.None
 })
 export class TableLightComponent implements OnInit {
 
