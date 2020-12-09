ec58bc56584cbdecf95cd22e9b6ca7cf1a991771
Jean-Christophe AUDARD <jc.audard@hotmail.fr>
2020-12-09 11:03:19 +0100

    Ajout d'une Directive ColorState dans le module shared, pour gérer les couleurs de l'état de l'order selon cet état

---
+++ appFormation/src/app/shared/directives/color-state.directive.ts
@@ -0,0 +1,25 @@
+import { parseHostBindings } from '@angular/compiler';
+import { Directive, HostBinding, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Directive({
+  selector: '[appColorState]'
+})
+export class ColorStateDirective implements OnChanges {
+
+  // Propriété @input du même nom que notre selector de Directive
+  @Input() appColorState:string;
+
+  // Décorateur pour maj les classes CSS
+  @HostBinding('class') className:string;
+
+  constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.className = this.formatClass(this.appColorState);
+  }
+
+  // Renvoie le nom d'une classe CSS state-<state> à partir d'un state de type string
+  private formatClass(state:string):string {
+   return `state-${state.replace(/\s/g,'').toLocaleLowerCase()}`;
+  }
+}
--- appFormation/src/app/shared/shared.module.ts
+++ appFormation/src/app/shared/shared.module.ts
@@ -3,18 +3,20 @@ import { CommonModule, getLocaleDayPeriods } from '@angular/common';
 import { TableLightComponent } from './components/table-light/table-light.component';
 import { TotalPipe } from './pipes/total.pipe';
 import { JoursPipe } from './pipes/jours.pipe';
+import { ColorStateDirective } from './directives/color-state.directive';
 
 
 
 @NgModule({
-  declarations: [TableLightComponent, TotalPipe, JoursPipe],
+  declarations: [TableLightComponent, TotalPipe, JoursPipe, ColorStateDirective],
   imports: [
     CommonModule
   ],
   exports: [
     TableLightComponent,
     JoursPipe,
-    TotalPipe
+    TotalPipe,
+    ColorStateDirective
   ]
 })
 export class SharedModule { }
