c226fee8beeddb3567c182feafb226abb2ed5935
Jean-Christophe AUDARD <jc.audard@hotmail.fr>
2020-12-10 12:26:51 +0100

    Mise en gras/italique des titres des pages qui s'appuie sur le composant TitreB

--- appFormation/src/app/texts/global-text/global-text.component.ts
+++ appFormation/src/app/texts/global-text/global-text.component.ts
@@ -9,6 +9,7 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 export class GlobalTextComponent implements OnInit {
 
   @Input() public page:boolean;
+  @Input() public pageClient:boolean;
 
   constructor() { }
 
